16ad92c007bff96a76fbaaa0ab7da52b2ace0848
Carlos Filipe <thecarlosfilipe@gmail.com>
2023-05-21 22:02:10 -0300

    Update classes

--- desktop/src/main/java/com/barbershop/model/domain/VendaItem.java
+++ desktop/src/main/java/com/barbershop/model/domain/VendaItem.java
@@ -4,6 +4,8 @@
  */
 package com.barbershop.model.domain;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author carlos
@@ -15,6 +17,56 @@ public class VendaItem {
     private Integer quantidade;
     private BigDecimal total;
     private BigDecimal desconto;
-    
+
+    public VendaItem() {
+    }
+
+    public VendaItem(Venda venda, Produto produto, Integer quantidade, BigDecimal total, BigDecimal desconto) {
+        this.venda = venda;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.total = total;
+        this.desconto = desconto;
+    }
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public BigDecimal getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(BigDecimal desconto) {
+        this.desconto = desconto;
+    }
     
 }
